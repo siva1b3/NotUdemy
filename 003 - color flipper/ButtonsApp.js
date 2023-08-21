03694fe1376f5a935d950ac1fba61d221229e6e8
siva1b3 <siva1b3@gmail.com>
2023-08-21 17:31:35 +0530

    11

--- 003 - color flipper/ButtonsApp.js
+++ 003 - color flipper/ButtonsApp.js
@@ -7,80 +7,65 @@ const RGB = document.querySelector("#RGB");
 const HSL = document.querySelector("#HSL");
 const WITHALPHA = document.querySelector("#WITHALPHA");
 
-COLORFLIPPER.addEventListener("click", () => toggleButton("COLORFLIPPER"));
-NAMED.addEventListener("click", () => toggleButton("NAMED"));
-HEX.addEventListener("click", () => toggleButton("HEX"));
-RGB.addEventListener("click", () => toggleButton("RGB"));
-HSL.addEventListener("click", () => toggleButton("HSL"));
-WITHALPHA.addEventListener("click", () => toggleButton("WITHALPHA"));
+COLORFLIPPER.addEventListener("click", () =>
+  toggleButtonClass(COLORFLIPPER, "btn-outline-primary", "btn-primary")
+);
+NAMED.addEventListener("click", () =>
+  toggleButtonClass(NAMED, "btn-outline-secondary", "btn-secondary")
+);
+HEX.addEventListener("click", () =>
+  toggleButtonClass(HEX, "btn-outline-success", "btn-success")
+);
+RGB.addEventListener("click", () =>
+  toggleButtonClass(RGB, "btn-outline-info", "btn-info")
+);
+HSL.addEventListener("click", () =>
+  toggleButtonClass(HSL, "btn-outline-dark", "btn-dark")
+);
+WITHALPHA.addEventListener("click", () =>
+  toggleButtonClass(WITHALPHA, "btn-outline-warning", "btn-warning")
+);
 
-function toggleButton(buttonName) {
-  switch (buttonName) {
-    case "COLORFLIPPER":
-      COLORFLIPPER.classList.toggle("btn-outline-primary");
-      COLORFLIPPER.classList.toggle("btn-primary");
-      break;
-    case "NAMED":
-      NAMED.classList.toggle("btn-outline-secondary");
-      NAMED.classList.toggle("btn-secondary");
-      break;
-    case "HEX":
-      HEX.classList.toggle("btn-outline-success");
-      HEX.classList.toggle("btn-success");
-      break;
-    case "RGB":
-      RGB.classList.toggle("btn-outline-info");
-      RGB.classList.toggle("btn-info");
-      break;
-    case "HSL":
-      HSL.classList.toggle("btn-outline-dark");
-      HSL.classList.toggle("btn-dark");
-      break;
-    case "WITHALPHA":
-      WITHALPHA.classList.toggle("btn-outline-warning");
-      WITHALPHA.classList.toggle("btn-warning");
-      break;
-    default:
-      break;
-  }
-}
-
-COLORFLIPPER.addEventListener("mouseover", () => showToolTip("COLORFLIPPER"));
-NAMED.addEventListener("mouseover", () => showToolTip("NAMED"));
-HEX.addEventListener("mouseover", () => showToolTip("HEX"));
-RGB.addEventListener("mouseover", () => showToolTip("RGB"));
-HSL.addEventListener("mouseover", () => showToolTip("HSL"));
-WITHALPHA.addEventListener("mouseover", () => showToolTip("WITHALPHA"));
-
-COLORFLIPPER.addEventListener("mouseout", () => hideToolTip("COLORFLIPPER"));
-NAMED.addEventListener("mouseout", () => hideToolTip("NAMED"));
-HEX.addEventListener("mouseout", () => hideToolTip("HEX"));
-RGB.addEventListener("mouseout", () => hideToolTip("RGB"));
-HSL.addEventListener("mouseout", () => hideToolTip("HSL"));
-WITHALPHA.addEventListener("mouseout", () => hideToolTip("WITHALPHA"));
-
-function showToolTip(buttonName) {
-  const tooltip = document.createElement("span");
-  tooltip.textContent = buttonName;
-  tooltip.setAttribute("id", "delete");
-  tooltip.classList.toggle("coupontooltip");
-  const button = document.querySelector(`#${buttonName}`);
-  const computedStyles = getComputedStyle(button);
+function toggleButtonClass(button, classToRemove, classToAdd) {
+  button.classList.toggle(classToRemove);
+  button.classList.toggle(classToAdd);
   if (button.classList.value.includes("outline")) {
-    tooltip.style.backgroundColor = "red";
+    button.classList.toggle("without-outline");
+    button.classList.toggle("with-outline");
   } else {
-    tooltip.style.backgroundColor =
-      computedStyles.backgroundColor !== "rgba(0, 0, 0, 0)"
-        ? computedStyles.backgroundColor
-        : "red";
+    button.classList.toggle("without-outline");
+    button.classList.toggle("with-outline");
   }
-  button.classList.toggle("couponcode");
-  button.appendChild(tooltip);
+  button.clicked = true;
+  console.log(button.clicked);
 }
 
-function hideToolTip(buttonName) {
-  const button = document.querySelector(`#${buttonName}`);
-  button.classList.toggle("couponcode");
-  const tooltip = document.querySelector(`#delete`);
-  tooltip.remove();
+COLORFLIPPER.addEventListener("mouseout", () => firstClick(COLORFLIPPER));
+NAMED.addEventListener("mouseout", () => firstClick(NAMED));
+HEX.addEventListener("mouseout", () => firstClick(HEX));
+RGB.addEventListener("mouseout", () => firstClick(RGB));
+HSL.addEventListener("mouseout", () => firstClick(HSL));
+WITHALPHA.addEventListener("mouseout", () => firstClick(WITHALPHA));
+
+function firstClick(button) {
+  if (button.clicked) {
+    button.clicked = false;
+    console.log("click happened");
+  } else {
+    console.log("only mosuse out no click");
+  }
 }
+
+// COLORFLIPPER.addEventListener("mouseover", () => showToolTip("COLORFLIPPER"));
+// NAMED.addEventListener("mouseover", () => showToolTip("NAMED"));
+// HEX.addEventListener("mouseover", () => showToolTip("HEX"));
+// RGB.addEventListener("mouseover", () => showToolTip("RGB"));
+// HSL.addEventListener("mouseover", () => showToolTip("HSL"));
+// WITHALPHA.addEventListener("mouseover", () => showToolTip("WITHALPHA"));
+
+// COLORFLIPPER.addEventListener("mouseout", () => hideToolTip("COLORFLIPPER"));
+// NAMED.addEventListener("mouseout", () => hideToolTip("NAMED"));
+// HEX.addEventListener("mouseout", () => hideToolTip("HEX"));
+// RGB.addEventListener("mouseout", () => hideToolTip("RGB"));
+// HSL.addEventListener("mouseout", () => hideToolTip("HSL"));
+// WITHALPHA.addEventListener("mouseout", () => hideToolTip("WITHALPHA"));

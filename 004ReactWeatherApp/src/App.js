ddf0191a62d2c8806443b9ac18fc868338ece2b4
siva1b3 <siva1b3@gmail.com>
2023-08-26 11:48:39 +0530

    Changes to be committed: 	new file:   004ReactWeatherApp/package.json 	new file:   004ReactWeatherApp/public/index.html 	new file:   004ReactWeatherApp/src/App.js 	new file:   004ReactWeatherApp/src/components/search/Search.js 	new file:   004ReactWeatherApp/src/index.js

---
+++ 004ReactWeatherApp/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Search from "./components/search/Search";
+
+export default function App() {
+  return (
+    <>
+      <div>siva naga babu</div>
+      <h2>hi</h2>
+    </>
+  );
+}
---
+++ 004ReactWeatherApp/src/components/search/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+
+export default function Search(props) {
+  return (
+    <>
+      <AsyncPaginate />
+    </>
+  );
+}

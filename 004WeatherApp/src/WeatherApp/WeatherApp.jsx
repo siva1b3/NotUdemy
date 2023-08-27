6bab08f8f4b170d911b8b7e17176cf837685a045
siva1b3 <siva1b3@gmail.com>
2023-08-27 19:25:18 +0530

    Changes to be committed: 	new file:   004WeatherApp/src/WeatherApp/Search.jsx 	modified:   004WeatherApp/src/WeatherApp/WeatherApp.jsx

--- 004WeatherApp/src/WeatherApp/WeatherApp.jsx
+++ 004WeatherApp/src/WeatherApp/WeatherApp.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import search_icon from "../Assets/search.png";
+import clear_icon from "../Assets/clear.png";
+import cloud_icon from "../Assets/cloud.png";
+import snow_icon from "../Assets/snow.png";
+import drizzle_icon from "../Assets/drizzle.png";
+import humidity_icon from "../Assets/humidity.png";
+import rain_icon from "../Assets/rain.png";
+import wind_icon from "../Assets/wind.png";
+import Search from './Search';
 
 const WeatherApp = () => {
   return (
     <div>
       <h1>Weather App</h1>
+      <search />
     </div>
   )
 }
 
-export default WeatherApp
+export default WeatherApp;

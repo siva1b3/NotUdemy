6e23d33917701496738dc8abd2311dc43cf603cd
siva1b3 <siva1b3@gmail.com>
2023-09-03 10:54:03 +0530

    added

---
+++ 004DashBoard/src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./App.css";
+import BarChart from "./Layout/AllCharts";
+import App01 from "./ExtraBarchart/NewBar";
+import RandomiseValue from "./Data/Random";
+
+export default function App() {
+  const [stateOfFiltersJson, setStateOfFiltersJson] = useState({
+    Homes: "All placements",
+    Age: ["0-3", "4-6", "7-12", "13-15", "16-above"],
+  });
+
+  const RandomValuesJson = RandomiseValue();
+
+  function changesInJson(Key, Value) {
+    if (Key === "Homes") {
+      setStateOfFiltersJson((prevState) => {
+        return { ...prevState, Homes: Value };
+      });
+    } else if (Key === "Age") {
+      setStateOfFiltersJson((prevState) => {
+        return { ...prevState, Age: Value };
+      });
+    }
+    console.log(stateOfFiltersJson);
+    console.log("hi in app");
+  }
+
+  // {
+  //   "Homes": "Home Needs",
+  //   "Age": "0-3",
+  //   "Region": "West",
+  //   "Gender": "Male",
+  //   "Race": "American Indian/Alsakan",
+  //   "PlacementType": "Foster",
+  //   "Value": "1",
+  //    "id":78
+  // }
+
+  let filteredjson;
+
+  filteredjson = RandomValuesJson.filter((item) => {
+    return item["Homes"] === stateOfFiltersJson["Homes"];
+  });
+  filteredjson = filteredjson.filter((item) => {
+    return stateOfFiltersJson["Age"].includes(item["Age"]);
+  });
+  if (filteredjson.length === 0) {
+    console.log("empty array");
+  }
+  const k = filteredjson.slice(0, 5);
+
+  return (
+    <div>
+      <BarChart
+        stateOfFiltersJson={stateOfFiltersJson}
+        changesInJson={changesInJson}
+        filteredjson={filteredjson}
+      />
+      {k.map((item, index) => {
+        return <p key={index}>{JSON.stringify(item)}</p>;
+      })}
+    </div>
+  );
+}
---
+++ 004DashBoard/src/Layout/AllCharts/ChartsCollection.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function ChartsCollection() {
+  return (
+    <div className="charts">
+      <div className="chart"></div>
+      <div className="chart"></div>
+      <div className="chart"></div>
+      <div className="chart"></div>
+      <div className="chart"></div>
+      <div className="chart"></div>
+    </div>
+  );
+}
+
+export default ChartsCollection;

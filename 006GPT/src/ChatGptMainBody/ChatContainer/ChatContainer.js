e2e0e798e1b49c7799b76e6d03a47dcc51a9f845
SivaDev <42565813+siva1b3@users.noreply.github.com>
2023-11-12 19:25:16 +0530

    new

--- 006GPT/src/ChatGptMainBody/ChatContainer/ChatContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import styles from "./ChatContainer.module.css";
-
-function ChatContainer(props) {
-  return <>ChatContainer from ChatContainer</>;
-}
-
-export default ChatContainer;
